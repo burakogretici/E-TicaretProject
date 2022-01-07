5386978d46b8aa8a4ed7ab1c7367c7a6b54d9afa
BURAK ÖĞRETİCİ <burakogreticii@gmail.com>
2022-01-08 00:38:23 +0300

    00 :38 => Base Class

---
+++ Core/Business/Abstract/IBaseService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Business.Abstract
+{
+    public interface IBaseService<T> 
+    {
+        IResult Add(T entity);
+        IResult Update(T entity);
+        IResult Delete(T entity);
+
+        IDataResult<List<T>> GetAll();
+        IDataResult<T> GetById(int id);
+
+    }
+}
