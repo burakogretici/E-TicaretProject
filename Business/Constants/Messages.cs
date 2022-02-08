34de2bbebd23df8f0271793ea746064e8ad762b5
BURAK ÖĞRETİCİ <burakogreticii@gmail.com>
2022-02-08 13:15:12 +0300

    edit

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-using Entities.Concrete;
-
-namespace Business.Constants
+﻿namespace Business.Constants
 {
     public static class Messages
     {
