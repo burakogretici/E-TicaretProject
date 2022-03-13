611bbde57b238c83948011d971c808fafa8ede68
Burak Öğretici <burakogreticii@gmail.com>
2022-03-13 15:07:02 +0300

    added

---
+++ Core/Entities/Concrete/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.Entities.Concrete
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; } 
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
