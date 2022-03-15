7b2ebfdb4d05a93cd6f0f942c9894b435e75d2c6
Burak Öğretici <burakogreticii@gmail.com>
2022-03-15 15:32:54 +0300

    api result added

---
+++ Core/Utilities/Results/ApiResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class ApiResult<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string InternalMessage { get; set; }
+        public T Data { get; set; }
+        public List<string> Errors { get; set; }
+    }
+
+    public class ApiReturn : ApiResult<object>
+    {
+    }
+}
