834045ca2ce74ef4aca9a27a8aff8c5e14d7ac34
Burak Öğretici <burakogreticii@gmail.com>
2022-03-21 15:40:56 +0300

    added

---
+++ Business/Rules/BrandRules.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+
+namespace Business.Rules
+{
+    public class BrandRules
+    {
+        private readonly IBrandDal _brandDal;
+
+        public BrandRules(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+
+        public IResult BrandNameAlreadyExists(string brandName)
+        {
+            var result =  _brandDal.AnyAsync(b => b.Name == brandName).Result;
+            if (result)
+            {
+                return new ErrorResult(Messages.BrandNameAlreadyExists);
+            }
+
+            return new SuccessResult();
+        }
+
+    }
+}
---
+++ Business/Rules/ColorRules.cs
@@ -0,0 +1,24 @@
+﻿using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+
+namespace Business.Rules
+{
+    public class ColorRules
+    {
+        private readonly IColorDal _colorDal;
+
+        public ColorRules(IColorDal colorDal) => _colorDal = colorDal;
+
+        private IResult ColorNameAlreadyExists(string colorName)
+        {
+            var result = _colorDal.AnyAsync(c => c.Name == colorName).Result;
+            if (result)
+            {
+                return new ErrorResult(Messages.ColorNameAlreadyExists);
+            }
+
+            return new SuccessResult();
+        }
+    }
+}
