8544f87a8286457d15053dfda0646b658d9883a8
Burak Öğretici <burakogreticii@gmail.com>
2022-05-01 01:06:52 +0300

    generic fixed

--- Core/Entities/Concrete/BaseEntity.cs
+++ Core/Entities/Concrete/BaseEntity.cs
@@ -6,7 +6,7 @@ namespace Core.Entities.Concrete
     public abstract class BaseEntity : BaseEntity<Guid> { }
     public abstract class BaseEntity<T> : IEntity
     {
-        public T Id { get; set; }
+        public  virtual T Id { get; set; }
 
 
         //public bool IsActive { get; set; }
