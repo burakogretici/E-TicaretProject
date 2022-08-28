77e6250406e22f0cf25866898f40ca34094a1062
Burak Öğretici <burak.ogretici@omicron.com.tr>
2022-08-28 14:45:13 +0300

    base dto updated

--- Entities/Dtos/BaseDto.cs
+++ Entities/Dtos/BaseDto.cs
@@ -6,5 +6,8 @@ namespace Entities.Dtos
     public class BaseDto : IDto
     {
         public Guid Id { get; set; }
+        public bool Deleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
     }
 }
