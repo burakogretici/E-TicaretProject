8f0d3a7bc6a7da5a17f66ecfb8a4f6d18c596d4d
Burak Öğretici <burak.ogretici@omicron.com.tr>
2022-09-13 15:46:23 +0300

    updated

---
+++ Business/Handlers/Cities/Commands/CreateCityCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.UnitOfWork;
+using Entities.Concrete;
+using MediatR;
+
+namespace Business.Handlers.Cities.Commands
+{
+    public class CreateCityCommand : IRequest<IResult>
+    {
+        public Guid CountryId { get; set; }
+        public string Name { get; set; }
+
+        public class CreateCityCommandHandler : IRequestHandler<CreateCityCommand, IResult>
+        {
+            private readonly IUnitOfWork _unitOfWork;
+            private readonly IMapper _mapper;
+
+            public CreateCityCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+            {
+                _unitOfWork = unitOfWork;
+                _mapper = mapper;
+            }
+
+            public async Task<IResult> Handle(CreateCityCommand request, CancellationToken cancellationToken)
+            {
+                var mapper = _mapper.Map<City>(request);
+                await _unitOfWork.CityRepository.AddAsync(mapper);
+                await _unitOfWork.Commit();
+                return new SuccessResult(Messages.CityAdded);
+            }
+        }
+    }
+}
---
+++ Business/Handlers/Cities/Commands/UpdateCityCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.UnitOfWork;
+using Entities.Concrete;
+using MediatR;
+
+namespace Business.Handlers.Cities.Commands
+{
+    public class UpdateCityCommand : IRequest<IResult>
+    {
+        public Guid Id { get; set; }
+        public Guid CountryId { get; set; }
+        public string Name { get; set; }
+
+        public class UpdateCityCommandHandler : IRequestHandler<UpdateCityCommand, IResult>
+        {
+            private readonly IUnitOfWork _unitOfWork;
+            private readonly IMapper _mapper;
+
+            public UpdateCityCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+            {
+                _unitOfWork = unitOfWork;
+                _mapper = mapper;
+            }
+
+            public async Task<IResult> Handle(UpdateCityCommand request, CancellationToken cancellationToken)
+            {
+                City city = await _unitOfWork.CityRepository.GetAsync(x => x.Id == request.Id);
+                if (city != null)
+                {
+                    city.Name = request.Name;
+                    city.CountryId = request.CountryId;
+                }
+                await _unitOfWork.CityRepository.UpdateAsync(city);
+                await _unitOfWork.Commit();
+                return new SuccessResult(Messages.CityUpdated);
+            }
+        }
+    }
+}
